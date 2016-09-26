2b1dfb2393f70b83943d4fb5d05de8eb5e85c363
Chester VanDuzer <sumgai@protonmail.com>
2016-09-26 19:30:46 +0000

    Changed new action in profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,7 +3,7 @@ class ProfilesController < ApplicationController
   def new
     # Form where each user can fill out their own profile.
     @user = User.find( params[:user_id] )
-    @profile = @user.build_profile
+    @profile = Profile.new
   end
     
   def create
