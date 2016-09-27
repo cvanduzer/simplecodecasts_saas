618b3b78d93d418be93d1e9312b536eb1f63d6bc
Chester VanDuzer <sumgai@protonmail.com>
2016-09-27 00:34:29 +0000

    Added before filters to check correct user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,6 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :only_current_user
   
   def new
     # Form where each user can fill out their own profile.
@@ -10,7 +12,7 @@ class ProfilesController < ApplicationController
     @user = User.find( params[:user_id] )
     @profile = @user.build_profile(profile_params)
     if @profile.save
-      flash[:success] = "Profile Updated!"
+      flash[:success] = "Profile Created!"
       redirect_to user_path(params[:user_id])
     else
       flash[:danger] = "Profile not created! Please try again."
@@ -20,11 +22,28 @@ class ProfilesController < ApplicationController
   
   def edit
     @user = User.find( params[:user_id] )
-    @profile = User.profile
+    @profile = @user.profile
+  end
+  
+  def update
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
+    if @profile.update_attributes(profile_params)
+      flash[:success] = "Profile Updated!"
+      redirect_to user_path(params[:user_id])
+    else
+      flash[:danger] = "Profile not updated! Please try again."
+      render action: :edit
+    end
   end
   
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+    end
+    
+    def only_current_user
+      @user = User.find( params[:user_id] )
+      redirect_to(root_url) unless @user == current_user
     end
 end
